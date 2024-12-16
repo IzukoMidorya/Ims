3d85892a589dc808a57b3b89146fe97f6b09668f
abz <progtechdept@gmail.com>
2024-12-17 06:15:08 +0800

    Initial commit

---
+++ itemIndex.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
+const port = process.env.PORT || 5000;
+const app = express();
+const userRoutes = require('./server/routes/itemRoutes');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.use('/items', userRoutes);
+
+app.listen(port, (req, res) => {
+  console.log(`Serving up on ${port}`);
+});
